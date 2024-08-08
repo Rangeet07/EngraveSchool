f1af6b2602ee2a0f2458241aa7572fa2f087489b
Rangeet07 <rangeetnandy07@gmail.com>
2024-08-09 02:23:07 +0530

    coursecommit4

--- features/Home/components/HeroForm/HeroForm.js
+++ features/Home/components/HeroForm/HeroForm.js
@@ -1,6 +1,8 @@
 import { Box, Button, Flex, Link, Text } from '@chakra-ui/react'
 import React from 'react'
 import {PhoneIcon} from '@chakra-ui/icons'
+import { FaWhatsappSquare } from "react-icons/fa";
+import { IoCallOutline } from "react-icons/io5";
 
 
 
@@ -12,6 +14,7 @@ const HeroForm = () => {
         borderRadius="sm"
         backgroundColor="black"
         color="white"
+        
     >
         <Text fontSize="xl" fontWeight="bold">
         "Uncover Your Creative Spirit"
@@ -20,8 +23,8 @@ const HeroForm = () => {
         <Flex gap={{base:"0" , sm: "1rem"}} flexDirection={{base:'column', sm: "row"}}>
         <Button  colorScheme="purple"  type="submit"  width="50%" fontSize="xl" padding="2rem" marginTop="2rem"> Book Now </Button>
         <Flex flexDirection='column' marginTop="2rem">
-        <Link href='https://wa.me/9434810330' color='white' src=''  > Book on Whatsapp</Link>
-        <Link href='https://wa.me/9434810330' color='teal.600' src='' >Book on Call</Link>
+        <Link href='https://wa.me/9434810330' color='white' src=''  > <FaWhatsappSquare/>Book on Whatsapp</Link>
+        <Link href='https://wa.me/9434810330' color='teal.600' src='' ><IoCallOutline/>Book on Call</Link>
         </Flex>
       
 
